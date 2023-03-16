fcd9b17b013a175a631dc13f781549dd70402a3e
Dozie2001 <davidchidozie402@gmail.com>
2023-03-16 23:40:16 +0100

    New File

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""A class User that inherits from BaseModel"""
+"""A module that contains a User Class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
